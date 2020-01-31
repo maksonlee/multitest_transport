8ca513af998844b2ae0020dd1717c24cbb652857
Google Team <no-reply@google.com>
2020-01-30 16:14:13 -0800

    Internal change
    PiperOrigin-RevId: 292445812
    Change-Id: I2f14ae5174080014448f02a5cd8b65a309cd9016

--- multitest_transport/cli/cli_test.py
+++ multitest_transport/cli/cli_test.py
@@ -592,10 +592,8 @@ class CliTest(parameterized.TestCase):
     cli.Stop(args, self._CreateHost())
 
     self.mock_context.assert_has_calls([
-        mock.call.Run(['docker', 'kill', '-s', 'TERM', 'mtt'],
-                      timeout=command_util._DOCKER_KILL_CMD_TIMEOUT_SEC),
-        mock.call.Run(['docker', 'container', 'wait', 'mtt'],
-                      timeout=command_util._DOCKER_WAIT_CMD_TIMEOUT_SEC),
+        mock.call.Run(['docker', 'stop', 'mtt'],
+                      timeout=command_util._DOCKER_STOP_CMD_TIMEOUT_SEC),
         mock.call.Run(['docker', 'inspect', 'mtt'], raise_on_failure=False),
         mock.call.Run(['docker', 'container', 'rm', 'mtt']),
     ])
@@ -665,21 +663,19 @@ class CliTest(parameterized.TestCase):
         mock.call.Run(['systemctl', 'disable', 'mttd.service']),
     ])
 
-  @mock.patch('__main__.cli.command_util.DockerHelper.Wait')
+  @mock.patch('__main__.cli.command_util.DockerHelper.Stop')
   @mock.patch('__main__.cli.command_util.DockerHelper.IsContainerRunning')
   @mock.patch.object(cli, '_ForceKillMttNode')
-  def testStop_ForceKill(self, force_kill, is_running, docker_wait):
+  def testStop_ForceKill(self, force_kill, is_running, docker_stop):
     is_running.return_value = True
     self.mock_context.host = 'ahost'
     args = self.arg_parser.parse_args(['stop'])
-    docker_wait.return_value = command_util.CommandResult(
-        -9, None, 'command timed out')
+    docker_stop.return_value = command_util.CommandResult(
+        1, None, 'command timed out')
 
     cli.Stop(args, self._CreateHost())
 
     self.mock_context.assert_has_calls([
-        mock.call.Run(['docker', 'kill', '-s', 'TERM', 'mtt'],
-                      timeout=command_util._DOCKER_KILL_CMD_TIMEOUT_SEC),
         mock.call.Run(['docker', 'inspect', 'mtt'], raise_on_failure=False),
         mock.call.Run(['docker', 'container', 'rm', 'mtt']),
     ])
