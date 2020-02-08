ea3a6403b03f27e43d70becc35b59f6f969f400a
Daniel Peykov <peykov@google.com>
2020-02-07 17:03:57 -0800

    Internal change
    PiperOrigin-RevId: 293925961
    Change-Id: I7b55534b16be5670e4d3eb25b35ae74a306b162f

--- multitest_transport/core/app.py
+++ multitest_transport/core/app.py
@@ -17,12 +17,8 @@ import google3
 
 import webapp2
 
-from google.appengine.ext import deferred
-from google.appengine.ext import ndb
-
 from multitest_transport.core import config_loader
 from multitest_transport.core import cron_kicker
-from multitest_transport.models import ndb_models
 from multitest_transport.test_scheduler import download_util
 from multitest_transport.test_scheduler import test_scheduler
 from multitest_transport.util import tfc_client
@@ -32,11 +28,6 @@ class AppStartHandler(webapp2.RequestHandler):
   """App start event handler."""
 
   def get(self):
-    # TODO: remove once users have initialized summaries
-    oldest_summary = ndb_models.TestRunSummary.query().order(
-        ndb_models.TestRunSummary.create_time).get()
-    deferred.defer(_InitTestRunSummaries, starting_from=oldest_summary)
-
     config_loader.Load()
     cron_kicker.Init()
 
@@ -49,20 +40,6 @@ class AppStartHandler(webapp2.RequestHandler):
 
     # Requeue or Cancel the pending test runs.
     test_scheduler.CheckPendingTestRuns()
-
-
-def _InitTestRunSummaries(starting_from=None, cursor=None, batch_size=100):
-  """Initialize test run summaries."""
-  query = ndb_models.TestRun.query().order(-ndb_models.TestRun.create_time)
-  if starting_from:
-    query = query.filter(
-        ndb_models.TestRun.create_time < starting_from.create_time)
-  test_runs, next_cursor, more = query.fetch_page(
-      batch_size, start_cursor=cursor)
-  summaries = [test_run.ToSummary() for test_run in test_runs]
-  ndb.put_multi(summaries)
-  if more:
-    deferred.defer(_InitTestRunSummaries, cursor=next_cursor)
 
 
 APP = webapp2.WSGIApplication([
