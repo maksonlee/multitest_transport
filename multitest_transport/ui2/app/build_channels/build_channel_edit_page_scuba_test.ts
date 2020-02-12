817026a47757d878241dcdc39a8b098ac70c6210
Di Qian <diqian@google.com>
2020-02-12 15:57:22 -0800

    Internal change
    PiperOrigin-RevId: 294778374
    Change-Id: Iefa88aaccdc31e5d357659954cd5c87e2e94dd58

---
+++ multitest_transport/ui2/app/build_channels/build_channel_edit_page_scuba_test.ts
@@ -0,0 +1,77 @@
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {ActivatedRoute} from '@angular/router';
+import {RouterTestingModule} from '@angular/router/testing';
+import {beforeEach, bootstrapTemplate, describe, it, setupModule} from 'google3/javascript/angular2/testing/catalyst';
+import {of as observableOf} from 'rxjs';
+
+import {MttClient} from '../services/mtt_client';
+import {KarmaTestEnv} from '../testing/karma_env';
+import {BuildChannelsModule} from './build_channels_module';
+import {BuildChannelsModuleNgSummary} from './build_channels_module.ngsummary';
+
+describe('BuildChannelEditPage scuba test', () => {
+  const env = new KarmaTestEnv({
+    scubaGoldensPath:
+        'third_party/py/multitest_transport/ui2/app/build_channels/scuba_goldens',
+    axe: false,
+  });
+
+  let mttClient: jasmine.SpyObj<MttClient>;
+  let liveAnnouncer: jasmine.SpyObj<LiveAnnouncer>;
+
+  beforeEach(() => {
+    liveAnnouncer =
+        jasmine.createSpyObj('liveAnnouncer', ['announce', 'clear']);
+    mttClient = jasmine.createSpyObj(
+        'mttClient', ['getBuildChannel', 'getBuildChannelProviders']);
+
+    setupModule({
+      imports: [BuildChannelsModule, NoopAnimationsModule, RouterTestingModule],
+      summaries: [BuildChannelsModuleNgSummary],
+      providers: [
+        {provide: MttClient, useValue: mttClient},
+        {
+          provide: ActivatedRoute,
+          useValue: {params: observableOf({'id': '123'})},
+        },
+        {provide: LiveAnnouncer, useValue: liveAnnouncer},
+      ],
+    });
+  });
+
+  it.async('can display a form with no option', async () => {
+    const BUILD_CHANNEL_CONFIG = {
+      id: 'google_drive',
+      name: 'Test Google Drive',
+      provider_name: 'Google Drive',
+    };
+
+    const BUILD_CHANNEL_PROVIDERS = [{name: 'Google Drive'}];
+    mttClient.getBuildChannel.and.returnValue(
+        observableOf(BUILD_CHANNEL_CONFIG));
+    mttClient.getBuildChannelProviders.and.returnValue(
+        observableOf({build_channel_providers: [...BUILD_CHANNEL_PROVIDERS]}));
+    bootstrapTemplate(`<build-channel-edit-page></build-channel-edit-page>`);
+    await env.verifyState(
+        `build-channel-edit-page-no-option`, 'build-channel-edit-page');
+  });
+
+  it.async('can display a form with option', async () => {
+    const BUILD_CHANNEL_CONFIG = {
+      id: 'google_cloud_storage',
+      name: 'Test Google Cloud Storage',
+      provider_name: 'Google Cloud Storage',
+      options: [{name: 'account_id', value: '3333333'}],
+    };
+
+    const BUILD_CHANNEL_PROVIDERS = [{name: 'Google Cloud Storage'}];
+    mttClient.getBuildChannel.and.returnValue(
+        observableOf(BUILD_CHANNEL_CONFIG));
+    mttClient.getBuildChannelProviders.and.returnValue(
+        observableOf({build_channel_providers: [...BUILD_CHANNEL_PROVIDERS]}));
+    bootstrapTemplate(`<build-channel-edit-page></build-channel-edit-page>`);
+    await env.verifyState(
+        `build-channel-edit-page-with-option`, 'build-channel-edit-page');
+  });
+});
