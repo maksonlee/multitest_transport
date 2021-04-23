c896dfa2f8aad43cf151dc517dd16903b05ef199
Moon Kim <moonk@google.com>
2021-04-23 10:57:46 -0700

    Internal change
    PiperOrigin-RevId: 370114320
    Change-Id: I2281d61d89ef5ecbdc74e66fe28b9c1ea8e69465

--- multitest_transport/ui2/app/test_runs/attempt_status_test.ts
+++ multitest_transport/ui2/app/test_runs/attempt_status_test.ts
@@ -18,34 +18,43 @@ import {DebugElement} from '@angular/core';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
-import {TestRun} from '../services/mtt_models';
-import {CommandAttempt} from '../services/tfc_models';
-import {getTextContent} from '../testing/jasmine_util';
-import {newMockCommandAttempt, newMockTestRun} from '../testing/mtt_mocks';
 import {AttemptStatus} from './attempt_status';
+import {APP_DATA, AppData} from '../services';
+import {Test, TestRun} from '../services/mtt_models';
+import {CommandAttempt, CommandState} from '../services/tfc_models';
+import {getTextContent} from '../testing/jasmine_util';
+import {newMockCommandAttempt, newMockTest, newMockTestRun} from '../testing/mtt_mocks';
+import {newMockAppData} from '../testing/mtt_lab_mocks';
 import {TestRunsModule} from './test_runs_module';
 import {TestRunsModuleNgSummary} from './test_runs_module.ngsummary';
 
 describe('AttemptStatus', () => {
   let attemptStatus: AttemptStatus;
-  let attemptStatusFixture: ComponentFixture<TestRunResults>;
+  let attemptStatusFixture: ComponentFixture<AttemptStatus>;
   let el: DebugElement;
 
+  let appData: AppData;
+  let test: Test;
   let testRun: TestRun;
-  let mockAttempt: CommandAttempt;
+  let attempt: CommandAttempt;
 
   beforeEach(() => {
-    testRun = newMockTestRun();
-    mockAttempt = newMockCommandAttempt();
+    appData = newMockAppData();
+    test = newMockTest();
+    testRun = newMockTestRun(test);
+    attempt = newMockCommandAttempt();
     TestBed.configureTestingModule({
       imports: [TestRunsModule, NoopAnimationsModule],
       aotSummaries: TestRunsModuleNgSummary,
+      providers: [
+        {provide: APP_DATA, useValue: appData},
+      ]
     });
     attemptStatusFixture = TestBed.createComponent(AttemptStatus);
     attemptStatus = attemptStatusFixture.componentInstance;
     el = attemptStatusFixture.debugElement;
     attemptStatus.testRun = testRun;
-    attemptStatus.attempt = mockAttempt;
+    attemptStatus.attempt = attempt;
     attemptStatusFixture.detectChanges();
   });
 
@@ -54,49 +63,47 @@ describe('AttemptStatus', () => {
   });
 
   it('can render queued attempt correctly', () => {
-    const attempt = newMockCommandAttempt();
-    attempt.state = CommandState.QUEUED;
+    const attempt = newMockCommandAttempt(
+        undefined, undefined, 'request_id', 'command_id', 'attempt_id',
+        CommandState.QUEUED);
     attemptStatus.attempt = attempt;
     attemptStatusFixture.detectChanges();
-    getTextContent(el).contains('Test is queued.');
+    getTextContent(el).includes('Test is queued.');
   });
 
   it('can render running attempt correctly', () => {
-    const attempt = newMockCommandAttempt();
-    attempt.state = CommandState.RUNNING;
+    const attempt = newMockCommandAttempt(
+        undefined, undefined, 'request_id', 'command_id', 'attempt_id',
+        CommandState.RUNNING);
     attemptStatus.attempt = attempt;
     attemptStatusFixture.detectChanges();
-    getTextContent(el).contains('Test is running.');
+    getTextContent(el).includes('Test is running.');
   });
 
   it('can render completed attempt correctly', () => {
-    const attempt = newMockCommandAttempt();
-    attempt.state = CommandState.COMPLETED;
-    attempt.failed_test_count = '0';
-    attempt.passed_test_count = '123';
-    attempt.total_test_count = '1234';
+    const attempt = newMockCommandAttempt(
+        0, 123, 'request_id', 'command_id', 'attempt_id',
+        CommandState.COMPLETED);
     attemptStatus.attempt = attempt;
     attemptStatusFixture.detectChanges();
-    getTextContent(el).contains('Test completed: 123 tests passed.');
+    getTextContent(el).includes('Test completed: 123 tests passed.');
   });
 
   it('can render completed attempt with failed test cases correctly', () => {
-    const attempt = newMockCommandAttempt();
-    attempt.state = CommandState.COMPLETED;
-    attempt.failed_test_count = '12';
-    attempt.passed_test_count = '123';
-    attempt.total_test_count = '1234';
+    const attempt = newMockCommandAttempt(
+        12, 123, 'request_id', 'command_id', 'attempt_id',
+        CommandState.COMPLETED);
     attemptStatus.attempt = attempt;
     attemptStatusFixture.detectChanges();
-    getTextContent(el).contains('Test completed: 12/135 tests passed.');
+    getTextContent(el).includes('Test completed: 12/135 tests passed.');
   });
 
   it('can render error attempt correctly', () => {
-    const attempt = newMockCommandAttempt();
-    attempt.state = CommandState.ERROR;
+    const attempt = newMockCommandAttempt(
+        0, 123, 'request_id', 'command_id', 'attempt_id',
+        CommandState.ERROR);
     attemptStatus.attempt = attempt;
-    attemptStatus.error = 'error';
     attemptStatusFixture.detectChanges();
-    getTextContent(el).contains('Test failed to start: error');
+    getTextContent(el).includes('Test failed to start: error');
   });
 });
